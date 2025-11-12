c0e21fed2282f1a56dd1767149fc3ff8d99a9593
ZhihanWang125 <zhihann.wang@mail.utoronto.ca>
2025-11-12 15:42:07 -0500

    Task 1.1.

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,9 +1,12 @@
 package theater;
 
+/**
+ * Class representing a  play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
